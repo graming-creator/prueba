017a9dc8ce0bcafabf07ec31176f35e3c2cfd3e8
graming <graming.212121@gmail.com>
2025-02-03 08:01:12 -0300

    first commit

--- config/connectDB.js
+++ config/connectDB.js
@@ -3,10 +3,11 @@ import mongoose, { mongo } from "mongoose";
 process.loadEnvFile()
 
 const URI_DB = process.env.URI_DB;
+const URL = process.env.URL
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(URI_DB)
+        await mongoose.connect(URL)
         console.log("Conexion exitosa con la base de datos!");
     } catch (error) {
         console.error(error)
